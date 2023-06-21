8876619770908ac8c6ffb5aa7667497533522951
SamanthaMk <samanthamkandwana17@gmail.com>
2023-06-20 17:03:05 -0700

    updated file 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,10 +6,9 @@
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero,-1 and prints - if n is less than zero
  */
-
 int print_sign(int n)
 {
-if(n >0)
+if(n > 0)
 {
 _putchar(43);
 return (1);
