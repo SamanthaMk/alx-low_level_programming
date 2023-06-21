5f2412a9e7f5ea768a795d90b856cee37e25d2cb
SamanthaMk <samanthamkandwana17@gmail.com>
2023-06-20 20:46:25 -0700

    updated file 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
- * print_sign - code outputs the sign of a number
- * @n: the int to check
+ * print_sign - This code outputs the sign of a number
+ * @n: is the parameters
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero,-1 and prints - if n is less than zero
  */
@@ -23,5 +22,4 @@ else
 _putchar(48);
 return (0);
 }
-
 }        	
