232d5eddc91fc203f8c073827c5827c041e243a3
SamanthaMk <samanthamkandwana17@gmail.com>
2023-06-20 18:30:10 -0700

    updated file name 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @c: Is the first value to be computed
+ * @b: Is secound value to be computed
+ * Return: sum of c and b
+ */
+int add(int c, int b)
+{
+	int d;
+
+         d = c + b;
+
+	return (d);
+}
