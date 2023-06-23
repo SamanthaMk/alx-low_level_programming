a8509ca43536a80b07bbd852c22d5a191d7d6318
SamanthaMk <samanthamkandwana17@gmail.com>
2023-06-22 18:49:50 -0700

    updated file 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,22 +1,39 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * fizzBuzz - Prints numbers from 1 to 100 according to the FizzBuzz rules.
- * Return: Value 0
+ * main - Prints numbers from 1 to 100 according to the FizzBuzz rules.
+ *
+ * Return: Always 0.
  */
-void fizzBuzz(void)
+int main(void)
 {
-int w;
-for (w = 1; w <= 100; w++)
-{
-if (w % 3 == 0 && w % 5 == 0)
-printf("FizzBuzz ");
-else if (w % 3 == 0)
-printf("Fizz ");
-else if (w % 5 == 0)
-printf("Buzz ");
-else
-printf("%d ", w);
-}
-printf("\n");
+	int f;
+
+	for (f = 1; f <= 100; f++)
+	{
+		if (f % 3 == 0 && f % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (f % 5 == 0 && f % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (f % 3 == 0 && f % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (f == 1)
+		{
+			printf("%d", f);
+		}
+		else
+		{
+			printf(" %d", f);
+		}
+	}
+
+	printf("\n");
+	return (0);
 }
