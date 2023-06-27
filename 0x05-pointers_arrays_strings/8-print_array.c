022b4cd7c84ce524dccb43cd062443749e8cd3dd
SamanthaMk <samanthamkandwana17@gmail.com>
2023-06-26 18:47:06 -0700

    updated file 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an array of integers
+ * @a: Array name
+ * @n: Number of elements to print
+ *
+ * Return: None
+ */
+void print_array(int *a, int n)
+{
+	int q;
+
+	for (q = 0; q < n - 1; q++)
+	{
+		printf("%d, ", a[q]);
+	}
+
+	if (q == n - 1)
+	{
+		printf("%d", a[q]);
+	}
+
+	printf("\n");
+}
