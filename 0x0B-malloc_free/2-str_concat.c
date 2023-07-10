90fcac29065164d9ab278662b19a0a065cfcd07b
SamanthaMk <samanthamkandwana17@gmail.com>
2023-07-10 16:49:39 -0700

    improved file called 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,33 +10,33 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int i, ci;
+	int i, pc;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	i = ci = 0;
+	i = pc = 0;
 	while (s1[i] != '\0')
 		i++;
-	while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
+	while (s2[pc] != '\0')
+		pc++;
+	conct = malloc(sizeof(char) * (i + pc + 1));
 
 	if (conct == NULL)
 		return (NULL);
-	i = ci = 0;
+	i = pc = 0;
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
 		i++;
 	}
 
-	while (s2[ci] != '\0')
+	while (s2[pc] != '\0')
 	{
-		conct[i] = s2[ci];
-		i++, ci++;
+		conct[i] = s2[pc];
+		i++, pc++;
 	}
 	conct[i] = '\0';
 	return (conct);
