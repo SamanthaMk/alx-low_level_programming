e51f00521f71596640cd2c7417af624135e87863
SamanthaMk <samanthamkandwana17@gmail.com>
2023-08-01 13:21:50 -0700

    improved file named 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,69 +1,35 @@
 #include "lists.h"
-#include <stdio.h>
 /**
- * looped_listint_len - Code will counts the number of unique nodes
- * in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
- * Return: If the list is not looped - 0.
- * Otherwise - the number of unique nodes in the list.
+ * free_listint_safe - Codes function frees a linked list
+ * @h: pointer to the first node in the linked list
+ *This function can free lists with a loop
+ * Return: number of elements in the freed list
  */
-size_t looped_listint_len(const listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
-const listint_t *tap, *he;
-size_t nodes = 1;
-if (head == NULL || head->next == NULL)
+size_t ln = 0;
+int df;
+listint_t *te;
+if (!h || !*h)
 return (0);
-tap = head->next;
-he = (head->next)->next;
-while (he)
+while (*h)
 {
-if (tap == he)
+df = *h - (*h)->next;
+if (df > 0)
 {
-tap = head;
-while (tap != he)
-{
-nodes++;
-tap = tap->next;
-he = he->next;
-}
-tap = tap->next;
-while (tap != he)
-{
-nodes++;
-tap = tap->next;
-}
-return (nodes);
-}
-tap = tap->next;
-he = (he->next)->next;
-}
-return (0);
-}
-/**
- * print_listint_safe - This will print a listint_t list safely.
- * @head: A pointer to the head of the listint_t list.
- * Return: The number of nodes in the list.
- */
-size_t print_listint_safe(const listint_t *head)
-{
-size_t nodes, index = 0;
-nodes = looped_listint_len(head);
-if (nodes == 0)
-{
-for (; head != NULL; nodes++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-}
+te = (*h)->next;
+free(*h);
+*h = te;
+ln++;
 }
 else
 {
-for (index = 0; index < nodes; index++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
+free(*h);
+*h = NULL;
+ln++;
+break;
 }
-printf("-> [%p] %d\n", (void *)head, head->n);
 }
-return (nodes);
+*h = NULL;
+return (ln);
 }
